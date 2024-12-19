976179bfc7304900b46245c11968d466c49e4315
Leo <leonardo.alvirubro@hotmail.com>
2024-12-19 18:04:30 -0300

    basic readfile cmd implemented to read names from a TXT file

--- cmd/ReadFile.go
+++ cmd/ReadFile.go
@@ -4,7 +4,9 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"bufio"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -17,11 +19,42 @@ var ReadFileCmd = &cobra.Command{
 	Short: "Ler um arquivo e sortear os pares",
 	Long:  `Essa funcao irá ler um arquivo TXT, sortear os pares do amigo secreto de forma aleatoria e gerar um arquivo TXT com os pares`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ReadFile called")
+		FunctionReadFile()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(ReadFileCmd)
 
+	ReadFileCmd.PersistentFlags().StringVarP(&filePath, "path", "p", "", "path to TXT file")
+	ReadFileCmd.MarkPersistentFlagRequired("path")
+
+}
+
+func FunctionReadFile() {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	// comecaremos a ler o arquivo aqui
+	scanner := bufio.NewScanner(f)
+
+	// slice nomes
+	var names []string
+	for scanner.Scan() {
+
+		line := scanner.Text()
+		names = append(names, line)
+	}
+
+	// checaar o erro aqui (posteriormente a verificaacao de impar)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// exibir os nomes para fim de exemplo, se funcionar fazer gerar um TXT de output
+	log.Println("Nomes lidos do arquivo:", names)
 }
