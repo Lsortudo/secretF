d3a44384af8ed10c06ac777ee6404bec12aabadd
Leo <leonardo.alvirubro@hotmail.com>
2024-12-19 17:51:05 -0300

    todo txt

--- cmd/ReadFile.go
+++ cmd/ReadFile.go
@@ -9,7 +9,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// ReadFileCmd represents the ReadFile command
+// declarar vars pra usar aqui em cima pra ficar + organizado
+var filePath string
+
 var ReadFileCmd = &cobra.Command{
 	Use:   "ReadFile",
 	Short: "Ler um arquivo e sortear os pares",
@@ -22,13 +24,4 @@ var ReadFileCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(ReadFileCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// ReadFileCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// ReadFileCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
